f6424c67ae536a0754d4454fd87c5c97dacb7895
Abhishek Gupta <83904358+Abhishek-gupta-1911@users.noreply.github.com>
2023-03-04 22:52:02 +0530

    add a program

---
+++ Milestone 2/Arrays & Lists/FindDuplicate.py
@@ -0,0 +1,40 @@
+# You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2).
+# Each number is present at least once. 
+# That is, if N = 5, the array/list constitutes values ranging from 0 to 3 and among these, 
+# there is a single integer value that is present twice. 
+# You need to find and return that duplicate number present in the array.
+
+
+import sys
+
+def duplicateNumber(arr, n) :
+    #Your code goes here
+    count = 0
+    for i in range(0, n):
+        if arr.count(arr[i]) > count:
+            count = arr.count(arr[i])
+            dn = arr[i]
+    return dn
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(sys.stdin.readline().strip())
+
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, sys.stdin.readline().strip().split()))
+    return arr, n
+
+
+#main
+t = int(sys.stdin.readline().strip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    print(duplicateNumber(arr, n))
+
+    t -= 1
+    
---
+++ Milestone 2/Arrays & Lists/PairSum.py
@@ -0,0 +1,37 @@
+# You have been given an integer array/list(ARR) and a number X. 
+# Find and return the total number of pairs in the array/list which sum to X.
+
+
+from sys import stdin
+
+def pairSum(arr, n, x) :
+    #Your code goes here
+    count = 0
+    i = 0
+    while i < n:
+        for j in range(i+1,n):
+            if arr[i]+arr[j] == x:
+                count += 1
+        i += 1
+    return count
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().strip())
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr, n
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    x = int(stdin.readline().strip())
+    print(pairSum(arr, n, x))
+
+    t -= 1
