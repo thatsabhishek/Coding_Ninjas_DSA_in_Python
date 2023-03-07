f24cf2b03b171392b92ba9ce520a3cd745aa77bc
Abhishek Gupta <83904358+Abhishek-gupta-1911@users.noreply.github.com>
2023-03-07 20:00:03 +0530

    add a program

---
+++ Milestone 2/Searching & Sorting/Pushzerostoend.py
@@ -0,0 +1,49 @@
+# You have been given a random integer array/list(ARR) of size N. You have been required to push all the zeros that are present in the array/list to the end of it. Also, make sure to maintain the relative order of the non-zero elements.
+
+# Input format :
+# The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+# First line of each test case or query contains an integer 'N' representing the size of the array/list.
+# Second line contains 'N' single space separated integers representing the elements in the array/list.
+
+from sys import stdin
+
+def pushZerosAtEnd(arr, n) :
+    #Your code goes here
+    nonZero = 0
+    for i in range(n):
+        if arr[i] != 0:
+            arr[i], arr[nonZero] = arr[nonZero], arr[i]
+            nonZero += 1
+    return arr
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().rstrip())
+
+    if n == 0:
+        return list(), 0
+    
+    arr = list(map(int, stdin.readline().rstrip().split()))
+    return arr, n
+  
+
+#to print the array/list
+def printList(arr, n) : 
+    for i in range(n) :
+        print(arr[i], end = " ")
+
+    print()
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+
+    arr, n = takeInput()
+
+    pushZerosAtEnd(arr, n)
+    printList(arr, n)
+
+    t -= 1
